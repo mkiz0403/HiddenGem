d94b7170403266de1e832af34601ed38fe13b5a9
Taejeong <mkiz0403@gmail.com>
2025-04-10 19:05:54 +0900

    feat : 로그인 페이지에 InputField 적용

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,81 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+
+import InputField from '../../components/InputField';
 
 export default function LoginScreen() {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  // const handleChangeEmail = (text: string) => {
+  //   setEmail(text);
+  // };
+
+  // const handleChangePassword = (text: string) => {
+  //   setPassword(text);
+  // };
+
+  //  신박한 useState 사용법
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeText = (name: string, text: string) => {
+    setValues({
+      ...values,
+      [name]: text,
+    });
+    setTouched({
+      ...touched,
+      [name]: false,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린 입니다.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={'이메일을 입력해주세요.'}
+          touched={touched.email}
+          inputMode="email"
+          value={values.email}
+          onChangeText={text => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={'비밀번호를 입력해주세요.'}
+          touched={touched.password}
+          value={values.password}
+          onChangeText={text => handleChangeText('password', text)}
+          secureTextEntry
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
 });
