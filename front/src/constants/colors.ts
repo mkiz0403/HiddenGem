9d8ecbe61208ce2e1625c870e26a09bb0cec9b8b
Taejeong <mkiz0403@gmail.com>
2025-04-10 19:05:01 +0900

    feat : inputFiled 컴포넌트 구현

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,5 +1,11 @@
 export const colors = {
   PINK_700: '#C63B64',
   PINK_500: '#BF5C79',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
   WHITE: '#fff',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
+  BLACK: '#000',
 };
